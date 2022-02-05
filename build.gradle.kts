776354edf7715c755f06c34e1bb5be90cd17f241
Marc Knaup <marc@knaup.io>
2022-02-05 20:22:00 +0100

    🎉 0.11.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 	id("io.fluidsonic.gradle") version "1.1.25"
 }
 
-fluidLibrary(name = "i18n", version = "0.10.0")
+fluidLibrary(name = "i18n", version = "0.11.0")
 
 fluidLibraryModule(description = "Kotlin multiplatform internationalization library") {
 	targets {
