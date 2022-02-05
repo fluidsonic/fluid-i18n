4fa8f739412416a88d2a998a36a758508e80b11d
Marc Knaup <marc@knaup.io>
2022-02-05 20:21:48 +0100

    Updated all dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import io.fluidsonic.gradle.*
 import org.jetbrains.kotlin.gradle.plugin.*
 
 plugins {
-	id("io.fluidsonic.gradle") version "1.1.23"
+	id("io.fluidsonic.gradle") version "1.1.25"
 }
 
 fluidLibrary(name = "i18n", version = "0.10.0")
@@ -11,12 +11,12 @@ fluidLibraryModule(description = "Kotlin multiplatform internationalization libr
 	targets {
 		common {
 			dependencies {
-				implementation(fluid("country", "0.10.0"))
-				implementation(fluid("currency", "0.10.1"))
+				implementation(fluid("country", "0.11.0"))
+				implementation(fluid("currency", "0.11.0"))
 				implementation(project(":fluid-i18n-data-identifiers"))
 				implementation(project(":fluid-i18n-data-regions"))
 
-				api(fluid("locale", "0.10.0"))
+				api(fluid("locale", "0.11.0"))
 			}
 		}
 
